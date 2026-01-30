67fea0e6d5e1a2825934b211947768238f05e315
Niponpal <niponpal52@gmail.com>
2026-01-30 21:46:01 +0600

    Fix Login Filed

--- lib/Auth/login.dart
+++ lib/Auth/login.dart
@@ -11,14 +11,50 @@ class _LoginScrenViewState extends State<LoginScrenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffFFFFFF),
-      body: Column(
-        children: [
-          SizedBox(height: 30,),
-          Center(child: Text("Login",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700),)),
+      backgroundColor: const Color(0xffFFFFFF),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            const Center(
+              child: Text(
+                "Login",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+              ),
+            ),
+            const SizedBox(height: 40),
 
+            const Text(
+              "User Email",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 6),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: 'Enter your email',
+                prefixIcon: Icon(Icons.email_outlined),
+              ),
+            ),
 
-        ],
+            const SizedBox(height: 50),
+
+            const Text(
+              "Password",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 6),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Enter your password',
+                prefixIcon: Icon(Icons.lock_outline),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

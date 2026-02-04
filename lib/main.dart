33ce4b6ee768ed54babc07ee60315d82ddd0f16c
Niponpal <niponpal52@gmail.com>
2026-02-04 10:48:19 +0600

    Fix All 🚀🚀🚑

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:jopmales/Auth/login.dart';
 import 'package:jopmales/Auth/register.dart';
 import 'package:jopmales/splash/ui.dart';
 import 'View/HOmescreenView.dart';
+import 'splash/ui.dart';
 
 void main (){
   runApp(
